47d6f646d98b22b40c92552d8cec6f845aa64076
Brian Gunther <brian.gunther@outlook.com>
2024-09-27 20:09:31 -0700

    Updated NameComparator.java

--- src/NameComparator.java
+++ src/NameComparator.java
@@ -1,4 +1,12 @@
+import java.util.Comparator;
 
-public class NameComparator {
-
+/**
+ * Comparator class to be used in comparing Student names
+ */
+public class NameComparator implements Comparator<Student> {
+	// Override the Comparator class "compare()" method to return the result of comparing two strings
+	@Override
+	public int compare(Student first, Student second) {
+		return first.getName().compareTo(second.getName());
+	}
 }
